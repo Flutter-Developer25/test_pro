70c1663269761ceaa97d458ee679e964e7d77dd2
Flutter-Developer25 <Flutter-Developer25@users.noreply.github.com>
2025-04-05 12:17:08 +0000

    deploy: 8a033487a7020e68817d82cb1d6a7c6067d2bd45

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1416884414"
+    serviceWorkerVersion: "4132685936"
   }
 });
